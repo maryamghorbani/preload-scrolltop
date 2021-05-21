91518b862845aa50edf97df3ddd4a927deb5c507
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-21 17:58:26 +0430

    preloader work correctly

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let preloader = document.getElementById('preloader');
+    let content = document.getElementById('content');
+
+    preloader.style.display = 'none';
+    content.style.display = 'block';
+});
