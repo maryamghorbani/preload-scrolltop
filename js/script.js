9fe202f238bba6faeec27f052c0e495db6e359cf
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-21 18:10:33 +0430

    show back to top element when scrolling

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
     preloader.style.display = 'none';
     content.style.display = 'block';
 });
+
+const backToTop = document.getElementById('backToTop')
+
+window.addEventListener('scroll', function (e) {
+    if (document.documentElement.scrollTop > 250) {
+        backToTop.style.display = 'flex'
+    } else {
+        backToTop.style.display = 'none'
+    }
+})
